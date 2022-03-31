acde92964deeaf9958328b9c6a2900d545897392
Liza T <eliza.tripolskaya21@gmail.com>
2022-03-31 16:47:30 +0300

    Changed hw.py on branch issue53

--- hw.py
+++ hw.py
@@ -5,3 +5,5 @@ master_3 = "Третий коммит"
 develop_1 = "Первый коммит в ветке develop"
 develop_2 = "Второй коммит в ветке develop"
 develop_3 = "Третий коммит в ветке develop"
+
+issue53_1 = "Первый коммит в ветке issue53"
